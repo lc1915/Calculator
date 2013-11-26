67099361799154055c2a38e71319f95e0e0843aa
lc1915 <liuchang1915@gmail.com>
2013-11-26 23:35:26 +0800

    加了括号和注释 可是括号还没有弄好。。

--- src/com/unique/calculator/Calculate.java
+++ src/com/unique/calculator/Calculate.java
@@ -5,186 +5,186 @@ import java.util.Map;
 import java.util.HashMap;
 import java.util.Stack;
 
-/**
- * @author Administrator
- *
- */
 public class Calculate {
-     
-     private static final Map<String, Integer> OPERATORS = new HashMap<String, Integer>();
-     
-     static {
-            OPERATORS.put( "+", 0);
-            OPERATORS.put( "-", 0);
-            OPERATORS.put( "x", 1);
-            OPERATORS.put( "/", 1);
-     }
-     
-     private static boolean isOperator(String str) {
-            return OPERATORS.containsKey(str);
-     }
-     
-     // Compare operator precedence.
-     //   Returns negative number is op1 has lower precedence.
-     //   Returns 0 if their precedence is equal.
-     //   Returns positive number is op1 has greater precedence.
-     private static final int comparePrecedence(String op1, String op2) {
-            if (! isOperator(op1) || !isOperator(op2)) {
-                 throw new IllegalArgumentException( "Invalid operators:" + op1 + " " + op2);
-                
-           }
-           
-            return OPERATORS.get(op1) - OPERATORS.get(op2);
-     }
-     
-     // Split the infix notation and store the operands and operators in the returned array.
-     //   E.g., the input infixExp = "2.3+5.8"
-     //   Returns: [0] = "2.3"
-     //               [1] = "+"
-     //               [2] = "5.8"
-     private static String[] splitInfixExp(String infixExp) {
-           
-           ArrayList<String>    strArr = new ArrayList<String>();
-           
-           String     lastOperand = "";
-           
-            for ( char ch: infixExp.toCharArray()) {
-                String     str = Character. toString(ch);
-                 if ( isOperator(str)) {
-                     
-                      // Check that lastOperand must have some characters.
-                         //                Note that we allow negative values, such as -4.5 + 2, 
-                         //                we need to take care of the leading negative sign.
-                      if (lastOperand == "") {
-                              if (str.equals("-")){
-                                      lastOperand += ch;
-                                      continue;
-                              }
-                              
-                            //throw new IllegalArgumentException("Invalid infix expression:" + infixExp);
-                              return null;
-                      }
-                           
-                     strArr.add(lastOperand);
-                     strArr.add(str);
-                     
-                     lastOperand = "";
-                }
-                 else {
-                     lastOperand += ch;
-                }
-           }
-           
-            // Add the last operand
-           strArr.add(lastOperand);
-           
-           String[] outputs = new String[strArr.size()];
-            return strArr.toArray(outputs);
-     }     // splitInfixExp
-     
-     // Convert an infix notation to RPN notation.
-     //   E.g. Infix : 2.6+5.8
-     //          RPN: 2.6 5.8 +
-     //   Note that the operands and operators in the RPN will be separated by spaces.
-     private static String infix2RPN(String infixExp) {
-           String rpnExp = "";
-           
-           String[] inputs = splitInfixExp(infixExp);
-           
-           if (inputs == null)
-                   return null;
-           
-           Stack<String> stack = new Stack<String>();
-           
-            for (String input: inputs) {
-                 if ( isOperator(input)) {
-                      // The string is an operator, check the stack to see
-                      //   if the operator precedence is less than or equal to
-                      //   the top operators in the stack, if so pop all such
-                      //   operators until the stack is empty or see the operator
-                      //   with lower precedence.
-                      while (!stack.empty() && isOperator(stack.peek())) {
-                            if ( comparePrecedence(input, stack.peek()) <= 0) {
-                                rpnExp += " ";
-                                rpnExp += stack.peek();
-                                stack.pop();
-                                 continue;
-                           }
-                            break;
-                     }
-                
-                      // Push the new operator onto the stack.
-                     stack.push(input);
-                }
-                 else {
-                     rpnExp += " ";
-                     rpnExp += input;
-                }
-           }
-           
-            while (!stack.empty()) {
-                rpnExp += " ";
-                rpnExp += stack.pop();
-           }
-           
-            return rpnExp.trim();
-     } // infix2RPN
-     
-     // Evaluates an RPN expression:
-     //   E.g., 2.3 5.6 +
-     private static double evalRPN(String rpnExp) {
-            double result = 0.0;
-           
-           String[] inputs = rpnExp.split( " ");
-           
-           Stack<Double> stack = new Stack<Double>();
-            for (String op: inputs) {
-                 if( isOperator(op)) {
-                      if (stack.size() < 2)
-                            //throw new IllegalArgumentException("Invalid RPN expression:" + rpnExp);
-                              return -1.0;
-                     
-                     Double val2 = stack.pop();
-                     Double val1 = stack.pop();
-                     Double valLoc = 0.0;
-                     
-                      if (op.equals( "+"))
-                           valLoc = val1 + val2;
-                      else if (op.equals( "-"))
-                           valLoc = val1 - val2;
-                      else if (op.equals( "x"))
-                           valLoc = val1 * val2;
-                      else if (op.equals( "/"))
-                           valLoc = val1 / val2;
-                      else
-                            //throw new IllegalArgumentException("Invalid RPN expression:" + rpnExp);
-                              System.out.println("Invalid RPN expression:" + rpnExp);
-                     
-                     stack.push(valLoc);
-                }
-                 else {
-                      try {
-                           Double val = Double. parseDouble(op);
-                           stack.push(val);
-                     }
-                      catch(NumberFormatException ex) {
-                          System.out.println( "NumberFormatException: " + ex.getMessage());
-                     }
-                }
-           }
-           
-           result = stack.pop();
-            if (!stack.empty())
-                 throw new IllegalArgumentException( "Invalid RPN expression:" + rpnExp);
-            return result;
-     }
-     
-     // Evaluate an infix expression.
-     public static double evalExp(String exp) {
-             String rpnExp = infix2RPN(exp);
-             if (rpnExp == null)
-                     return 0.0;
-             return evalRPN(rpnExp);
-     }
+
+	private static final Map<String, Integer> OPERATORS = new HashMap<String, Integer>();
+
+	static {
+		OPERATORS.put("+", 0);
+		OPERATORS.put("-", 0);
+		OPERATORS.put("x", 1);
+		OPERATORS.put("/", 1);
+		OPERATORS.put("(", 2);
+		OPERATORS.put(")", -1);
+	}
+
+	private static boolean isOperator(String str) {
+		return OPERATORS.containsKey(str);// 如果存在指定键的映射关系则返回true，否则返回false
+	}
+
+	// 比较运算符的优先级
+	private static final int comparePrecedence(String op1, String op2) {
+		if (!isOperator(op1) || !isOperator(op2)) {
+			// 抛出非法数据异常
+			throw new IllegalArgumentException("Invalid operators:" + op1 + " "
+					+ op2);
+		}
+		return OPERATORS.get(op1) - OPERATORS.get(op2);
+	}
+
+	// Split the infix notation and store the operands and operators in the
+	// returned array.
+	// E.g., the input infixExp = "2.3+5.8"
+	// Returns: [0] = "2.3"
+	// [1] = "+"
+	// [2] = "5.8"
+	private static String[] splitInfixExp(String infixExp) {
+
+		ArrayList<String> strArray = new ArrayList<String>();
+
+		String lastOperand = "";
+
+		for (char ch : infixExp.toCharArray()) { // toCharArray():将此字符串转换为一个新的字符数组
+			String str = Character.toString(ch);// Character 类在对象中包装一个基本类型 char
+												// 的值。Character 类型的对象包含类型为 char
+												// 的单个字段。
+
+			if (isOperator(str)) {
+				// Check that lastOperand must have some characters.
+				// Note that we allow negative values, such as -4.5 + 2,
+				// we need to take care of the leading negative sign.
+				if (lastOperand == "") {
+					if (str.equals("-")) {
+						lastOperand += ch;
+						continue;
+					}
+					// throw new
+					// IllegalArgumentException("Invalid infix expression:" +
+					// infixExp);
+					return null;
+				}
+
+				strArray.add(lastOperand);
+				strArray.add(str);
+
+				lastOperand = "";
+			} else {
+				lastOperand += ch;
+			}
+		}
+
+		// Add the last operand
+		strArray.add(lastOperand);
+
+		String[] outputs = new String[strArray.size()];
+		return strArray.toArray(outputs);
+	} // splitInfixExp
+
+	// 中缀表达式转化为后缀表达式
+	// Note that the operands and operators in the RPN will be separated by
+	// spaces.
+	private static String infix2RPN(String infixExp) {
+		String rpnExp = "";
+
+		String[] inputs = splitInfixExp(infixExp);
+
+		if (inputs == null)
+			return null;
+
+		Stack<String> stack = new Stack<String>();
+
+		for (String input : inputs) {
+			if (isOperator(input)) { // 如果是运算符
+
+				while (!stack.empty() && isOperator(stack.peek())) {
+					if (comparePrecedence(input, stack.peek()) <= 0
+							&& stack.peek() != "(") {
+						rpnExp += " ";
+						rpnExp += stack.peek();
+						stack.pop();// 栈顶元素出栈
+						continue;
+					}
+					if (stack.peek() == "(" && input == ")") {
+						stack.pop();
+						break;
+					}
+					break;
+
+				}
+
+				stack.push(input);
+			} else {
+				rpnExp += " ";
+				rpnExp += input;
+			}
+		}
+
+		while (!stack.empty()) {
+			rpnExp += " ";
+			rpnExp += stack.pop();
+		}
+
+		return rpnExp.trim();
+	}
+
+	// 计算后缀表达式
+	private static double evalRPN(String rpnExp) {
+		double result = 0.0;
+
+		String[] inputs = rpnExp.split(" ");
+
+		Stack<Double> stack = new Stack<Double>();
+		for (String op : inputs) {
+			if (isOperator(op)) {
+				if (stack.size() < 2)
+					// throw new
+					// IllegalArgumentException("Invalid RPN expression:" +
+					// rpnExp);
+					return -1.0;
+
+				Double val2 = stack.pop();
+				Double val1 = stack.pop();
+				Double valLoc = 0.0;
+
+				if (op.equals("+"))
+					valLoc = val1 + val2;
+				else if (op.equals("-"))
+					valLoc = val1 - val2;
+				else if (op.equals("x"))
+					valLoc = val1 * val2;
+				else if (op.equals("/"))
+					valLoc = val1 / val2;
+				else
+					// throw new
+					// IllegalArgumentException("Invalid RPN expression:" +
+					// rpnExp);
+					System.out.println("Invalid RPN expression:" + rpnExp);
+
+				stack.push(valLoc);
+			} else {
+				try {
+					Double val = Double.parseDouble(op);
+					stack.push(val);
+				} catch (NumberFormatException ex) {
+					System.out.println("NumberFormatException: "
+							+ ex.getMessage());
+				}
+			}
+		}
+
+		result = stack.pop();
+		if (!stack.empty())
+			throw new IllegalArgumentException("Invalid RPN expression:"
+					+ rpnExp);
+		return result;
+	}
+
+	// 计算中缀表达式
+	public static double evalExp(String exp) {
+		String rpnExp = infix2RPN(exp);
+		if (rpnExp == null)
+			return 0.0;
+		return evalRPN(rpnExp);
+	}
 
 }
--- src/com/unique/calculator/MainActivity.java
+++ src/com/unique/calculator/MainActivity.java
@@ -23,19 +23,20 @@ public class MainActivity extends Activity {
 
 	private boolean isOperator(char ch) {
 		boolean bOperator = false;
-		if (ch == '+' || ch == '-' || ch == 'x' || ch == 'X' || ch == '/')
+		if (ch == '+' || ch == '-' || ch == 'x' || ch == '/' || ch == '('
+				|| ch == ')')
 			bOperator = true;
 
 		return bOperator;
 	}
 
 	private boolean appendDotValid(String exp) {
-		if (exp.equals(""))
+		if (exp.equals(""))// 如果输入框中空白
 			return true;
 
 		int expLen = exp.length();
-		for (int ii = expLen - 1; ii >= 0; --ii) {
-			char ch = exp.charAt(ii);
+		for (int i = expLen - 1; i >= 0; --i) {
+			char ch = exp.charAt(i);
 			if (isOperator(ch))
 				return true;
 			else if (ch == '.')
@@ -73,21 +74,21 @@ public class MainActivity extends Activity {
 					Button bn = (Button) arg0;
 					String bnText = bn.getText().toString();
 
-					TextView expressionTextView = (TextView) findViewById(R.id.showTextView);
-					String oldExpression = expressionTextView.getText()
-							.toString();
+					TextView showTextView = (TextView) findViewById(R.id.showTextView);
+					String oldExpression = showTextView.getText().toString();
 
 					char inputCh = bnText.charAt(bnText.length() - 1);
 					if (isOperator(inputCh)) {
-						if (oldExpression.equals(""))
+						if (oldExpression.equals(""))// 如果输入框中空白
 							return;
 
-						char lastCh = oldExpression.charAt(oldExpression
-								.length() - 1);
-						if (isOperator(lastCh))
-							return;
+						//char lastCh = oldExpression.charAt(oldExpression
+								//.length() - 1);
+						//if (isOperator(lastCh))// 如果输入框中最后一个字符是符号
+							//return;
 					}
 
+					// 如果输入的是"."，而输入框中从最后一个字符开始向前遍历还没有碰到符号的时候就出现了"."
 					if (inputCh == '.' && !appendDotValid(oldExpression)) {
 						return;
 					}
@@ -97,10 +98,10 @@ public class MainActivity extends Activity {
 						double result = Calculate.evalExp(oldExpression);
 						newExpression = Double.toString(result);
 					} else {
-						newExpression = oldExpression.concat(bnText);
+						newExpression = oldExpression.concat(bnText);// 连接在oldExpression字符串的后面
 					}
 
-					expressionTextView.setText(newExpression);
+					showTextView.setText(newExpression);
 				}
 			});
 
@@ -121,23 +122,24 @@ public class MainActivity extends Activity {
 		deleteButton.setTextSize(oneSeventhHeight / 7);
 		showTextView = (TextView) findViewById(R.id.showTextView);
 		showTextView.setTextSize(oneSeventhHeight / 7);
-		
+
 	}
 
 	public void onClearText(View v) {
-		TextView expressionTextView = (TextView) findViewById(R.id.showTextView);
-		expressionTextView.setText("");
+		TextView showTextView = (TextView) findViewById(R.id.showTextView);
+		showTextView.setText("");
 	}
 
 	public void onDeleteText(View v) {
-		TextView expressionTextView = (TextView) findViewById(R.id.showTextView);
-		String oldExp = expressionTextView.getText().toString().trim();
+		TextView showTextView = (TextView) findViewById(R.id.showTextView);
+		String oldExp = showTextView.getText().toString().trim();// String.trim()返回字符串的副本，忽略前导空白和尾部空白(去掉两端的空白字符
+																	// 回车符啊制表符啊神马的)
 		if (oldExp.equals(""))
 			return;
 
-		// Remove the last character.
-		oldExp = oldExp.substring(0, oldExp.length() - 1);
-		expressionTextView.setText(oldExp);
+		// 删除最后一个字符
+		oldExp = oldExp.substring(0, oldExp.length() - 1);// substring(begin,end)：Begin到end之间,不包含end
+		showTextView.setText(oldExp);
 	}
 
 	@Override
